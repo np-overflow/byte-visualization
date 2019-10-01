c3605a5fd856f76ee3fb7b51df3ff62f6e4496e2
lczm <zemingchua@gmail.com>
2019-10-01 16:40:36 +0800

    Flask server can now serve react page

--- flask/main/__init__.py
+++ flask/main/__init__.py
@@ -1,11 +1,8 @@
 from flask import Flask
 
-import os
-print(os.getcwd())
-
 # app = Flask(__name__)
 # This is to set the file to automatically find the files react built
-app = Flask(__name__, static_folder="./build/static", template_folder="./build")
+app = Flask(__name__, static_folder="../build/static", template_folder="../build")
 app.config['SECRET_KEY'] = 'dev'
 
 from flask_sqlalchemy import SQLAlchemy
