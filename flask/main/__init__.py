b3c16281070cdfb9e031e6030d506be40a1dc86f
lczm <zemingchua@gmail.com>
2019-10-01 15:22:01 +0800

    Added create-react-app template and added build folders

--- flask/main/__init__.py
+++ flask/main/__init__.py
@@ -1,5 +1,11 @@
 from flask import Flask
-app = Flask(__name__)
+
+import os
+print(os.getcwd())
+
+# app = Flask(__name__)
+# This is to set the file to automatically find the files react built
+app = Flask(__name__, static_folder="./build/static", template_folder="./build")
 app.config['SECRET_KEY'] = 'dev'
 
 from flask_sqlalchemy import SQLAlchemy
--- flask/run.py
+++ flask/run.py
@@ -1,4 +1,10 @@
 from main import app
 
+from flask import render_template
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
