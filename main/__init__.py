bcfdccdcabb15fb390532eaa1aee9da0e4daea36
Yan Hwee Yee <s10186981@connect.np.edu.sg>
2019-10-01 11:31:27 +0800

    Inital commit

---
+++ main/__init__.py
@@ -0,0 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'dev'
+
+from flask_sqlalchemy import SQLAlchemy
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+from main import routes
