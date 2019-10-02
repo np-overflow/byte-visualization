ec859a99ae045de761f290f567f7d756b22d07b5
lczm <zemingchua@gmail.com>
2019-10-03 00:32:17 +0800

    Added uwsgi and nginx to communicate to each other, nginx proxies for uwsgi
    Added boilerplate testing fetch requests, so far it works

--- flask/run.py
+++ flask/run.py
@@ -1,4 +1,8 @@
 from main import app
 
+@app.route('/test', methods=['GET'])
+def test():
+    return "Test is working"
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,25 +2,45 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  componentDidMount() {
+
+    fetch('http://localhost/test')
+      .then(function (response) {
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+        // Do stuff with the response
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log('Looks like there was a problem: \n', error);
+
+      });
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
