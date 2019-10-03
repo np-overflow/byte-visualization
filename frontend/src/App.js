02321c60cae1fabe588c8eeea12d78c80dc8c097
lczm <zemingchua@gmail.com>
2019-10-03 20:08:38 +0800

    Eslint finally works

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,6 @@ import Plot from 'react-plotly.js';
 import './App.css';
 
 class App extends React.Component {
-
   componentDidMount() {
     fetch('http://localhost/test')
       .then((response) => {
@@ -35,11 +34,11 @@ class App extends React.Component {
             y: [2, 6, 3],
             type: 'scatter',
             mode: 'lines+points',
-            marker: {color: 'red'},
+            marker: { color: 'red' },
           },
-          {type: 'bar', x: [1, 2, 3], y: [2, 5, 3]},
+          { type: 'bar', x: [1, 2, 3], y: [2, 5, 3] },
         ]}
-        layout={ {width: 320, height: 240, title: 'A Fancy Plot'} }
+        layout={{ width: 800, height: 600, title: 'A Fancy Plot' }}
       />
     );
   }
