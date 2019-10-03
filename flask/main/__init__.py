7fb6fbd8ad5795bbb2fb4855458d3140ff663069
lczm <zemingchua@gmail.com>
2019-10-03 19:00:56 +0800

    Added flask-cors and plotly as dependencies

--- flask/main/__init__.py
+++ flask/main/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 app.config['SECRET_KEY'] = 'dev'
 
 from flask_sqlalchemy import SQLAlchemy
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
+import Plot from 'react-plotly.js';
+// import logo from './logo.svg';
+
 import './App.css';
 
 class App extends React.Component {
+
   componentDidMount() {
     fetch('http://localhost/test')
       .then((response) => {
@@ -25,27 +28,67 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit
-            <code>src/App.js</code>
-            and save to reload.
-          </p>
-
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Plot
+        data={[
+          {
+            x: [1, 2, 3],
+            y: [2, 6, 3],
+            type: 'scatter',
+            mode: 'lines+points',
+            marker: {color: 'red'},
+          },
+          {type: 'bar', x: [1, 2, 3], y: [2, 5, 3]},
+        ]}
+        layout={ {width: 320, height: 240, title: 'A Fancy Plot'} }
+      />
     );
   }
 }
+
+// class App extends React.Component {
+//   componentDidMount() {
+//     fetch('http://localhost/test')
+//       .then((response) => {
+//         if (response.status !== 200) {
+//           console.log(
+//             'Looks like there was a problem. Status Code: ' + response.status,
+//           );
+//           return;
+//         }
+
+//         // Examine the text in the response
+//         response.json().then((data) => {
+//           console.log(data);
+//         });
+//       })
+//       .catch((err) => {
+//         console.log('Fetch Error :-S', err);
+//       });
+//   }
+
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <p>
+//             Edit
+//             <code>src/App.js</code>
+//             and save to reload.
+//           </p>
+
+//           <a
+//             className="App-link"
+//             href="https://reactjs.org"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//           >
+//             Learn React
+//           </a>
+//         </header>
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
