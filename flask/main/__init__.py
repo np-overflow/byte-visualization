bed31fb3a0180b89af2c8a7c10f7ed8413e7be89
Yan Hwee Yee <s10186981@connect.np.edu.sg>
2019-10-15 10:43:59 +0800

    Changed sql connection string

--- flask/main/__init__.py
+++ flask/main/__init__.py
@@ -5,7 +5,7 @@ CORS(app)
 app.config['SECRET_KEY'] = 'dev'
 
 from flask_sqlalchemy import SQLAlchemy
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@db/'#'sqlite:///site.db'
 db = SQLAlchemy(app)
 
 from main import routes
