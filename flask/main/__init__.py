bc7c91a3b941196b08d3b08e6442cee1b3026413
Yan Hwee Yee <s10186981@connect.np.edu.sg>
2019-10-19 15:44:10 +0800

    Fixed Merge Conflicts __init__.py

--- flask/main/__init__.py
+++ flask/main/__init__.py
@@ -3,13 +3,8 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'dev'
 
 from flask_sqlalchemy import SQLAlchemy
-<<<<<<< HEAD
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@db/'
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-=======
-#'sqlite:///site.db'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@db/'
->>>>>>> ceec57fe88d95436f7831e752f85fd1a831dec4a
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 
 from flask_cors import CORS
