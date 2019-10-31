b5362de3ac7e3eb5f5083d9c872ec4013a47eddb
TooAwesome <yanhweeyee@gmail.com>
2019-10-31 10:16:11 +0800

    Edited config.py

--- worker/config.py
+++ worker/config.py
@@ -2,18 +2,8 @@ REPOS = [
     (
         (lambda user, repo: f'https://github.com/{user}/{repo}.git'),
         [
-            ('Tooawesome15', 'temp')
-        ]
-    ),
-    (
-        (lambda user, repo: f'https://github.com/{user}/{repo}.git'),
-        [
-            ('ossaris56', 'temp')
-        ]
-    ),
-    (
-        (lambda user, repo: f'https://github.com/{user}/{repo}.git'),
-        [
+            ('Tooawesome15', 'temp'),
+            ('ossaris56', 'temp'),
             ('lczm', 'temp')
         ]
     ),
