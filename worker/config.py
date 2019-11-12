65dbeacbc9559723ae40db5a2dd120f9c7434101
lczm <zemingchua@gmail.com>
2019-11-12 03:02:23 +0000

    test

--- worker/config.py
+++ worker/config.py
@@ -3,6 +3,8 @@ REPOS = [
         (lambda user, repo: f'https://github.com/{user}/{repo}.git'),
         [
             ('lczm', 'cosign'),
+            ('thenosewizard', 'unBiased'),
+            ('mrzzy', 'NP-Portfolio-2'),
         ]
     ),
     (
